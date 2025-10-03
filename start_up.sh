fa641c2e95ae048d7675626a1d6dee98343d2604
Eduardo Ormeño <eduardoorm79@gmail.com>
2025-10-03 19:12:39 +0200

    chore: update config package.json

--- start_up.sh
+++ start_up.sh
@@ -6,5 +6,7 @@ npx prisma migrate deploy
 # Ejecutar semillas de datos (si está configurado en package.json)
 npx prisma db seed
 
+npm run build
+
 # Iniciar la aplicación en modo producción
 npm run start:prod
