6c577ae0df53a7e017ec18dd44bc9f9cbde1fc4c
Eduardo Ormeño <eduardoorm79@gmail.com>
2025-10-03 21:57:20 +0200

    chore: cors main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://visma-frontend.web.app',
+      'https://visma-frontend.firebaseapp.com',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
