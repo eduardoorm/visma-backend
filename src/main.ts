26eda1e3c0c095bff5754d652c7a8794bd46b9cb
Eduardo Ormeño <eduardoorm79@gmail.com>
2025-10-03 18:56:07 +0200

    chore: update CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL || 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 8080);
+  const port = parseInt(process.env.PORT) || 8080;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
